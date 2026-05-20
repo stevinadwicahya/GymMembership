fd7a4543f8ad00456ceab4205ff224733df43661
DavinTanaya <davin.tanaya@gmail.com>
2026-05-20 14:07:56 +0700

    feat: register membership

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MembershipController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/api/membership', [MembershipController::class, 'register']);
